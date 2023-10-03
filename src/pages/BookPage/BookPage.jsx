4123196f2dc2ac26d6ec488a6a33805268b6e0cf
hebamesluh1 <hebamesluh@gmail.com>
2023-10-03 20:28:23 +0300

    fixed style

--- src/pages/BookPage/BookPage.jsx
+++ src/pages/BookPage/BookPage.jsx
@@ -8,10 +8,10 @@ const BookPage = () => {
     <div className="mb-10">
       <Title title="احجز الآن" />
       <div className="flex justify-between flex-col md:flex-row">
-        <div className="components basis-[60%]  p-3 ">
+        <div className="components basis-[60%]  p-3  fit-height">
           <Stepper />
         </div>
-        <div className="summary bg-footerBg w-full md:w-[30%] rounded-2xl max-h-fit">
+        <div className="summary bg-footerBg w-full md:w-[30%] rounded-2xl max-h-fit fit-height">
           <Summary />
         </div>
       </div>
